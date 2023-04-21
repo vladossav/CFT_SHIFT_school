4f5480d2e20f4def3da6c2e4e0ff030157fa9349
vlad <226973@mail.ru>
2023-04-21 15:55:00 +0700

    realized required part

---
+++ app/src/main/java/ru/savenkov/homework/BActivity.kt
@@ -0,0 +1,11 @@
+package ru.savenkov.homework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bactivity)
+    }
+}
